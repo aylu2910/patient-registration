66d632252a67da525f10f69b67659d00f6fe488e
Aylén Orellana <ayluorellana@gmail.com>
2025-01-01 18:21:47 +0100

    🔧 add TypeOrmModule root on AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'patients',
+      entities: [],
+      username: 'testuser',
+      password: 'testuser123',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
