65fedb60a8d5bde68ee6022d329492fc3819ddd4
Aylén Orellana <ayluorellana@gmail.com>
2025-01-02 15:03:40 +0100

    add mocks - patient for testing

---
+++ test/mocks/patient.js
@@ -0,0 +1,7 @@
+export const patientMock = {
+  name: 'John Doe',
+  email: 'Avenida@hotmail.com',
+  address: 'Avenida Siempre Viva 742',
+  phoneNumber: '+541166566299',
+  imageDocument: 'https://aws.cloud19357388dhg-5udtau1.com',
+};
