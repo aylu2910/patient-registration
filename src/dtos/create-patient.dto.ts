7493200d1ed686f0b0c1412195b56e3ecf2ef852
Aylén Orellana <ayluorellana@gmail.com>
2025-01-02 15:02:06 +0100

    rename create patient dto

--- src/dtos/create-patient.dto.ts
+++ src/dtos/create-patient.dto.ts
@@ -1,1 +1,29 @@
-export class CreatePatientDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsUrl,
+  IsEmail,
+  IsPhoneNumber,
+} from 'class-validator';
+
+export class CreatePatientDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  phoneNumber: string;
+
+  @IsNotEmpty()
+  @IsUrl()
+  imageDocument: string;
+}
