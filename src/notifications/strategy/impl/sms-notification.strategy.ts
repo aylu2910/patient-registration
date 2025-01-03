9e877f2b1867d2a6abb7d5fa622e2b2342dd31cc
Aylén Orellana <ayluorellana@gmail.com>
2025-01-03 12:13:01 +0100

    :building_construction: add mailer resource

--- src/notifications/strategy/impl/sms-notification.strategy.ts
+++ src/notifications/strategy/impl/sms-notification.strategy.ts
@@ -3,6 +3,6 @@ import { NotificationStrategy } from '../notification.strategy';
 export class SmsNotificationStrategy implements NotificationStrategy {
   async notify(recipient: string, message: string): Promise<void> {
     console.log(`Sending SMS to ${recipient} with message: ${message}`);
-    // Integrate your SMS service here (e.g., Twilio, Nexmo, etc.)
+    // Integrate SMS service like Twilio for example
   }
 }
