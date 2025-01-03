44f8b98683a7acd882e246da676331efacc9884e
Aylén Orellana <ayluorellana@gmail.com>
2025-01-03 12:28:01 +0100

    :sparkles: add logic mailer service

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  constructor(private readonly configService: ConfigService) {}
+  mailTransport() {
+    const transporter = nodemailer.createTransport({
+      host: this.configService.get('MAIL_HOST'),
+      port: this.configService.get('MAIL_PORT'),
+      secure: true,
+      auth: {
+        user: this.configService.get('MAIL_USER'),
+        pass: this.configService.get('MAIL_PASSWORD'),
+      },
+    });
+
+    return transporter;
+  }
+}
